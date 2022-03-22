f7d8c24f81f133524aaaa1f935afbf990c4b86db
Harsha Gihan Madhushankha Liyanage <57912882+harshagihan@users.noreply.github.com>
2022-03-22 15:18:20 +0530

    firebase

--- src/utils/firestore.js
+++ src/utils/firestore.js
@@ -1,18 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/firestore';
 
-const firebaseConfig = {
-	apiKey: 'AIzaSyCFNt82ILpuzsejXoBANWScT61DQVg-89w',
-	authDomain: 'hotel-menu-37c45.firebaseapp.com',
-	projectId: 'hotel-menu-37c45',
-	storageBucket: 'hotel-menu-37c45.appspot.com',
-	messagingSenderId: '347882697716',
-	appId: '1:347882697716:web:20b9311fa7c5c1b52e7fa1',
-	measurementId: 'G-XY7HV763GW',
-};
+// const firebaseConfig = {
+// 	apiKey: ,
+// 	authDomain: ,
+// 	projectId: ,
+// 	storageBucket: ,
+// 	messagingSenderId: ,
+// 	appId: ,
+// 	measurementId: ,
+// };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
-export { db };
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+// export { db };
